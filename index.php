9aacff38f7166eb799659584c96ab7ab41ec5d48
saranzafar <saranzafar736@gmail.com>
2024-10-19 10:45:34 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hello world!! <br>";
+echo "its really good";
+// this is comment
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <br>
+    <button>Order a pizza</button>
+</body>
+
+</html>
